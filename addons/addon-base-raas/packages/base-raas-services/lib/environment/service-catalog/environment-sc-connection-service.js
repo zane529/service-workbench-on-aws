bb673a19a43c8dfce5e0414e3e4b19b752c46186
Zane <18938064829@163.com>
2025-04-21 16:10:40 +0800

    update open jubpyter

--- addons/addon-base-raas/packages/base-raas-services/lib/environment/service-catalog/environment-sc-connection-service.js
+++ addons/addon-base-raas/packages/base-raas-services/lib/environment/service-catalog/environment-sc-connection-service.js
@@ -192,25 +192,7 @@ class EnvironmentScConnectionService extends Service {
       };
       const sageMakerResponse = await sagemaker.createPresignedNotebookInstanceUrl(params).promise();
 
-      // 修改为直接打开 JupyterLab 而不是 Jupyter Notebook
-      let authorizedUrl = _.get(sageMakerResponse, 'AuthorizedUrl');
-      if (authorizedUrl) {
-        // 检查 URL 是否已包含路径
-        if (authorizedUrl.includes('/tree?')) {
-          // 如果包含 /tree? 则替换为 /lab?
-          authorizedUrl = authorizedUrl.replace('/tree?', '/lab?');
-        } else {
-          // 如果不包含路径，则在查询参数前添加 /lab
-          const urlParts = authorizedUrl.split('?');
-          if (urlParts.length > 1) {
-            authorizedUrl = `${urlParts[0]}/lab?${urlParts[1]}`;
-          } else {
-            // 如果没有查询参数，直接添加 /lab
-            authorizedUrl = `${authorizedUrl}/lab`;
-          }
-        }
-      }
-      connection.url = authorizedUrl;
+      connection.url = _.get(sageMakerResponse, 'AuthorizedUrl');
     }
 
     if (
@@ -530,25 +512,6 @@ class EnvironmentScConnectionService extends Service {
       NotebookInstanceName: connection.info,
     };
     const sageMakerResponse = await sagemaker.createPresignedNotebookInstanceUrl(params).promise();
-    // 修改为直接打开 JupyterLab 而不是 Jupyter Notebook
-    let authorizedUrl = _.get(sageMakerResponse, 'AuthorizedUrl');
-    if (authorizedUrl) {
-      // 检查 URL 是否已包含路径
-      if (authorizedUrl.includes('/tree?')) {
-        // 如果包含 /tree? 则替换为 /lab?
-        authorizedUrl = authorizedUrl.replace('/tree?', '/lab?');
-      } else {
-        // 如果不包含路径，则在查询参数前添加 /lab
-        const urlParts = authorizedUrl.split('?');
-        if (urlParts.length > 1) {
-          authorizedUrl = `${urlParts[0]}/lab?${urlParts[1]}`;
-        } else {
-          // 如果没有查询参数，直接添加 /lab
-          authorizedUrl = `${authorizedUrl}/lab`;
-        }
-      }
-      sageMakerResponse.AuthorizedUrl = authorizedUrl;
-    }
     return sageMakerResponse;
   }
 
