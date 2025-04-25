83662caf575b0d9b3307e870307a7650b33c0bd4
Zane <18938064829@163.com>
2025-04-25 15:22:58 +0800

    update connect and start script.

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/parts/environments-builtin/EnvironmentConnectButton.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/parts/environments-builtin/EnvironmentConnectButton.js
@@ -31,8 +31,42 @@ class EnvironmentConnectButton extends React.Component {
     const environment = this.props.environment;
 
     const url = await this.getUrl(environment);
-    // Change to the notebook
-    newTab.location = url;
+    
+    // 如果是 SageMaker 环境，我们需要先加载原始 URL 进行认证，然后重定向到 JupyterLab
+    if (environment.instanceInfo && environment.instanceInfo.type === 'sagemaker') {
+      // 创建一个 HTML 页面，该页面会先加载原始 URL 进行认证，然后自动重定向到 JupyterLab
+      const redirectHtml = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Redirecting to JupyterLab...</title>
+          <script>
+            // 首先打开原始 URL 进行认证
+            const originalUrl = "${url}";
+            // 然后在页面加载完成后重定向到 JupyterLab
+            window.onload = function() {
+              // 等待短暂时间确保认证完成
+              setTimeout(function() {
+                window.location.href = originalUrl.replace('/tree', '/lab');
+              }, 100);
+            };
+            window.location.href = originalUrl;
+          </script>
+        </head>
+        <body>
+          <p>Redirecting to JupyterLab...</p>
+        </body>
+        </html>
+      `;
+      
+      // 将 HTML 写入新标签页
+      newTab.document.write(redirectHtml);
+      newTab.document.close();
+    } else {
+      // 对于非 SageMaker 环境，直接使用原始 URL
+      newTab.location = url;
+    }
+    
     environment.setFetchingUrl(false);
   };
 
